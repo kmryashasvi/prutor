9b4346dc2146cd1b9a412ace04e09ce7a55c23e6
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-03-26 07:58:01 +0530

    Create Hi Bye.py

---
+++ PuP0/Q2 (Hi Bye)/Hi Bye.py
@@ -0,0 +1,6 @@
+#TODO: A program to get input name of user and printing greetings
+
+
+name = input()               #TODO:Taking user input
+print('Hi', name)            #TODO:Printing hi with the input
+print('Bye',name)            #TODO:Printing bye with the input
